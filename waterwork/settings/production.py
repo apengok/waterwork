0fe5731a6e0e4339ec403561869f57d09c542eb6
apengok <apengok@163.com>
2018-06-21 01:53:38 +0800

    production

--- waterwork/settings/production.py
+++ waterwork/settings/production.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mptt',
     'entm',
     'accounts',
+    'monitor',
     # 'channels',
 ]
 
