e299a1337914c0f78222f07a4a2bd30720f07bda
apeng <apengok@163.com>
2019-05-28 17:38:30 +0800

    mysql gis

--- gis/models.py
+++ gis/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
+# from django.db import models
+from django.contrib.gis.db import models
 from django.urls import reverse
 from entm.models import Organizations
 import datetime
