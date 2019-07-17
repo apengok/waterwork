957f2d868b87dc8f3cddd44a68ec9e51a7e63b84
apeng <apengok@163.com>
2019-07-17 17:42:55 +0800

    api generics

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,10 +1,23 @@
 from django.conf.urls import url
 
 
-from .views import UserViewSet
+from .views import (
+    UserViewSet,
+    UserListView,
+    UserDetailView,
+    UserAllOpView,
+    UserCreateView,
+    UserUpdateView
+)
 
 app_name = 'api-account'
 urlpatterns = [
     url(r'^user/$', UserViewSet.as_view({'get':'list'}), name='user-listcreate'),
+    url(r'^user/list/$', UserListView.as_view(), name='user-list'),
+    url(r'^user/(?P<pk>\d+)/$', UserDetailView.as_view(), name='user-detail'),
+    url(r'^user/(?P<pk>\d+)/op/$', UserAllOpView.as_view(), name='user-allop'),
+    url(r'^user/(?P<pk>\d+)/update/$', UserUpdateView.as_view(), name='user-update'),
+    url(r'^user/create/$', UserCreateView.as_view(), name='user-create'),
+
     # url(r'^(?P<pk>\d+)/$', BlogPostRudView.as_view(), name='post-rud')
 ]   
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import  Group
 from accounts.models import User
 from rest_framework import viewsets
 from .serializers import UserSerializer
+from rest_framework import generics, mixins
+from rest_framework import permissions
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -13,3 +15,30 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 # Create your views here.
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class UserAllOpView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
